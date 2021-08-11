c8e84201ff823323be4d3178173c1cbde5d197c1
André Kuratomi <andrekuratomi@gmail.com>
2021-08-11 14:15:28 -0300

    feat: button component created as well as its style file.

---
+++ src/Components/Button/styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Button1 = styled.button`
+  background-color: var(--green);
+  border: 2px solid var(--light-green);
+  border-radius: 1rem;
+  color: var(--white);
+  padding: 0.5rem 2rem;
+`;
