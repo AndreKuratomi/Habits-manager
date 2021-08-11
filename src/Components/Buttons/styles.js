11a28897a990d0aa19397518471db724b0503068
Fernando Vieira <fernando.santos2901@gmail.com>
2021-08-11 16:54:21 +0000

    Revert "Merge branch 'feature/BUTTON' into 'develop'"
    This reverts merge request !4

--- src/Components/Buttons/styles.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const Button1 = styled.button`
-  background-color: var(--green);
-  border: 1px solid var(--light-green);
-  border-radius: 1rem;
-  color: var(--white);
-  padding: 0.5rem 2rem;
-`;
