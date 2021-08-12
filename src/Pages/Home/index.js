e75530e3871410a30964eaf30d8df094562f2cd1
Fernando Vieira <fernando.santos2901@gmail.com>
2021-08-12 11:53:58 -0300

    chore: update merge

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,6 +1,5 @@
 import Button from "../../Components/Button";
 import Card from "../../Components/Card";
-<<<<<<< HEAD
 import Header from "../../Components/Header";
 
 const Home = () => {
@@ -32,42 +31,5 @@ const Home = () => {
     </>
   );
 };
-=======
-import GroupsSubs from "../../Components/GroupsSubs";
-
-const Home = () => {
-    return (
-        <>
-            <h1>Home</h1>
-            <Card
-                title="Habito1"
-                category="category1"
-                achieved={true}
-                difficulty="Facil"
-                frequency="Diaria"
-                howMuch={30}
-            />
-            <Card
-                title="Habito2"
-                category="category2"
-                achieved={false}
-                difficulty="Dificil"
-                frequency="Semanal"
-                howMuch={100}
-            />
-            <Card
-                title="Meta"
-                difficulty="EZ"
-                howMuch={2}
-            />
-            <Card
-                title="Activity"
-                time="13:00"
-            />
-            <GroupsSubs />
-        </>
-    )
-}
->>>>>>> develop
 
 export default Home;
