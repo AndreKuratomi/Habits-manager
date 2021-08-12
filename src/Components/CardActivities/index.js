60ed0f71faad63345e934292cf4b771ec45bb382
miguel-maurer <miguel.maurer2001@gmail.com>
2021-08-12 13:57:48 -0300

    feat: card-activities

---
+++ src/Components/CardActivities/index.js
@@ -0,0 +1,22 @@
+import { Container } from "./style";
+
+
+const CardActivities = () => {
+
+
+    return (
+        <Container > 
+            <header>Atividades</header>
+            <div>
+                <p>Nome da atividade</p>
+                <span>Data pra realizacao</span>
+            </div>
+            <div>
+                <p>Ler capitulo 10</p>
+                <span>Prazo: 12/08/2021</span>
+            </div>
+        </Container>
+    )
+}
+
+export default CardActivities;
