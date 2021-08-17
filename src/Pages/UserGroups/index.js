b9aa75fe8d5d0e687acfe4b72cbb4dbab6a75a18
miguel-maurer <miguel.maurer2001@gmail.com>
2021-08-17 14:34:40 -0300

    feat: groups structure

--- src/Pages/UserGroups/index.js
+++ src/Pages/UserGroups/index.js
@@ -1,8 +1,55 @@
+import DashboardMenu from "../../Components/DashboardMenu";
+import Header from "../../Components/Header";
+import UserCard from "../../Components/UserCard";
+import GroupsSubs from "../../Components/GroupsSubs";
+import Button from "../../Components/Button";
+import { useState } from "react";
+
+import {
+  MainContainer,
+  MenuDashboard,
+  ListsContainer,
+  AsideContainer,
+  GroupsDiv,
+  ContainerHeader,
+} from "./../Dashboard/styles";
 
 const UserGroups = () => {
+
+  const [modalHabitsDisplay, setmodalHabitsDisplay] = useState(false);
+
+  const openModal = () => {
+    setmodalHabitsDisplay(true);
+  };
+  const closeModal = () => {
+    setmodalHabitsDisplay(false);
+  };
+
   return (
     <>
-      <h2>Grupos do usuario</h2>
+      <Header isLogged></Header>
+
+      <MainContainer>
+        <MenuDashboard>
+          <DashboardMenu />
+        </MenuDashboard>
+
+        <ListsContainer>
+          <ContainerHeader>
+            <h1>Grupos inscritos</h1>
+            <Button onClick={() => openModal()}>Novo</Button>
+          </ContainerHeader>
+        </ListsContainer>
+
+        <AsideContainer>
+          <div>
+            <UserCard />
+          </div>
+          <GroupsDiv>
+            <GroupsSubs />
+          </GroupsDiv>
+        </AsideContainer>
+      </MainContainer>
     </>
   );
 };
