b03825f74515f049398ed6e7a03bbea9cb5217f3
Fernando Vieira <fernando.santos2901@gmail.com>
2021-08-18 08:29:27 -0300

    style: doing identation

--- src/Components/CardActivities/index.js
+++ src/Components/CardActivities/index.js
@@ -1,22 +1,19 @@
 import { Container } from "./style";
 
-
 const CardActivities = () => {
-
-
-    return (
-        <Container > 
-            <header>Atividades</header>
-            <div>
-                <p>Nome da atividade</p>
-                <span>Data pra realizacao</span>
-            </div>
-            <div>
-                <p>Ler capitulo 10</p>
-                <span>Prazo: 12/08/2021</span>
-            </div>
-        </Container>
-    )
-}
+  return (
+    <Container>
+      <header>Atividades</header>
+      <div>
+        <p>Nome da atividade</p>
+        <span>Data pra realizacao</span>
+      </div>
+      <div>
+        <p>Ler capitulo 10</p>
+        <span>Prazo: 12/08/2021</span>
+      </div>
+    </Container>
+  );
+};
 
 export default CardActivities;
